1104460cc7ba3ddc93ef853468854c52ecf4513e
Paul Pillau <Paul.Online@Pillau.com>
2022-02-20 02:36:30 +0100

    Draggable selection working. Started Grid Stuff

--- src/components/BitList.js
+++ src/components/BitList.js
@@ -8,7 +8,8 @@ import classNames from "classnames/bind";
 const BitList = ({ areaId = 0, initialBitString }) => {
   const [cursorPosition, setCursorPosition, refCursorPosition] = useState(0);
   const [bitString, setBitString, refBitString] = useState(initialBitString);
-  const [dragSelector, setDragSelector] = useState({});
+  const [dragSelector, setDragSelector, refDragSelector] = useState({});
+  const [selection, setSelection] = useState([]);
 
   const moveCursorToEnd = useCallback(() => {
     moveCursor(refBitString.current.length * 2);
@@ -23,19 +24,43 @@ const BitList = ({ areaId = 0, initialBitString }) => {
   }, [refCursorPosition.current]);
 
   const moveCursor = (pos) => {
-    if (pos < 0 || pos > refBitString.current.length * 2) {
+    if (pos < 0 || pos > refBitString.current.length * 2 || pos % 2 !== 0) {
       return;
     }
     document.getElementById(`bitElement_${pos}`).focus();
   };
 
-  const deleteFromList = useCallback(
+  const deleteSingleFromList = useCallback(
     (pos) => {
       setBitString(
         refBitString.current.substr(0, pos - 1) +
           refBitString.current.substr(pos)
       );
-      moveCursorBackwards();
+    },
+    [setBitString]
+  );
+
+  const deleteMultipleFromList = useCallback(
+    (selection) => {
+      const selectionDirectionObject = findOutSelectionDirection(selection);
+      if (selectionDirectionObject.direction) {
+        const cropStart = Math.ceil(selectionDirectionObject.first / 2 - 1);
+        const cropEnd = Math.ceil(selectionDirectionObject.last / 2);
+        setBitString(
+          refBitString.current.substr(0, cropStart) +
+            refBitString.current.substr(cropEnd)
+        );
+        moveCursor(selectionDirectionObject.first - 1);
+      } else {
+        const cropStart = Math.ceil(selectionDirectionObject.first / 2 - 1);
+        const cropEnd = Math.ceil(selectionDirectionObject.last / 2);
+        setBitString(
+          refBitString.current.substr(0, cropStart) +
+            refBitString.current.substr(cropEnd)
+        );
+        moveCursor(selectionDirectionObject.first - 1);
+      }
+      refDragSelector.current.clearSelection();
     },
     [setBitString]
   );
@@ -48,6 +73,7 @@ const BitList = ({ areaId = 0, initialBitString }) => {
           refBitString.current.substr(pos)
       );
       moveCursorForwards();
+      refDragSelector.current.clearSelection();
     },
     [setBitString]
   );
@@ -63,7 +89,18 @@ const BitList = ({ areaId = 0, initialBitString }) => {
       //left
       moveCursorBackwards();
     } else if (e.which === 8) {
-      deleteFromList(refCursorPosition.current / 2);
+      if (refDragSelector.current.getSelection().length > 0) {
+        deleteMultipleFromList(refDragSelector.current.getSelection());
+      } else {
+        deleteSingleFromList(refCursorPosition.current / 2);
+        moveCursorBackwards();
+      }
+    } else if (e.which === 46) {
+      if (refDragSelector.current.getSelection().length > 0) {
+        deleteMultipleFromList(refDragSelector.current.getSelection());
+      } else {
+        deleteSingleFromList((refCursorPosition.current + 2) / 2);
+      }
     }
   }, []);
 
@@ -76,27 +113,72 @@ const BitList = ({ areaId = 0, initialBitString }) => {
   }, [handleKeyInput]);
 
   useEffect(() => {
-    setDragSelector(
-      new DragSelect({
-        selectables: document.querySelectorAll(".selectable_bits"),
-        area: document.getElementById(`bitlist_area_${areaId}`),
-        draggability: false,
-      })
-    );
+    const dragSelect = new DragSelect({
+      selectables: document.getElementsByClassName("selectable_bits"),
+      area: document.getElementById(`bitlist_area_${areaId}`),
+      draggability: false,
+    });
+    dragSelect.subscribe("callback", onSelection);
+    setDragSelector(dragSelect);
 
     moveCursor(cursorPosition);
   }, []);
 
   const onFocusTextArea = useCallback(
     (event, id) => {
-      setCursorPosition(id);
+      setCursorPosition(id ?? parseInt(event.target.id.split("_")[1]));
     },
     [setCursorPosition]
   );
 
+  const onSelection = useCallback(
+    (selectionRes) => {
+      if (
+        selectionRes &&
+        selectionRes.items &&
+        selectionRes.items.length &&
+        selectionRes.items.length > 0
+      ) {
+        const selectionDirectionObject = findOutSelectionDirection(
+          selectionRes.items
+        );
+
+        if (selectionDirectionObject.direction) {
+          moveCursor(selectionDirectionObject.last + 1);
+        } else {
+          moveCursor(selectionDirectionObject.first - 1);
+        }
+      }
+
+      setSelection(selectionRes.items);
+    },
+    [selection]
+  );
+
   const focusTextArea = (e) => {
-    e.preventDefault();
-    // moveCursorToEnd();
+    if (
+      e.relatedTarget ||
+      e.target.classList.contains("text_spacer") ||
+      refDragSelector.current.getSelection().length > 0
+    ) {
+      return;
+    }
+
+    moveCursorToEnd();
+  };
+
+  const findOutSelectionDirection = (selectionItems) => {
+    const first = parseInt(selectionItems[0].id.split("_")[1]);
+    const last = parseInt(
+      selectionItems[selectionItems.length - 1].id.split("_")[1]
+    );
+    let items = selectionItems.map((item) => parseInt(item.id.split("_")[1]));
+    items.sort((a, b) => a - b);
+    return {
+      direction: first < last,
+      first: items[0],
+      last: items[items.length - 1],
+    };
   };
 
   const renderBits = () => {
@@ -107,7 +189,7 @@ const BitList = ({ areaId = 0, initialBitString }) => {
     const firstInputElement = (
       <input
         maxLength="0"
-        className="text_spacer bit_cell"
+        className="text_spacer"
         type="text"
         dir="rtl"
         id={`bitElement_0`}
@@ -118,7 +200,7 @@ const BitList = ({ areaId = 0, initialBitString }) => {
       />
     );
 
-    for (const bit of bitString) {
+    bitString.split("").forEach((bit) => {
       if (["0", "1"].includes(bit)) {
         const bitElement = (
           <Bit id={`bitElement_${counter}`} key={counter}>
@@ -135,10 +217,7 @@ const BitList = ({ areaId = 0, initialBitString }) => {
             dir="rtl"
             id={`bitElement_${counter}`}
             key={counter}
-            onFocus={((e) => {
-              // e.preventDefault();
-              onFocusTextArea(counter, e);
-            }).bind(null, counter)}
+            onFocus={onFocusTextArea}
           />
         );
         counter++;
@@ -157,13 +236,18 @@ const BitList = ({ areaId = 0, initialBitString }) => {
         );
         cellCounter--;
       }
-    }
+    });
 
     if (renderBitsResult.length === 0) {
       renderBitsResult.push(
         <div key={cellCounter} className="cell bit_cell bit_cell_first">
           {firstInputElement}
         </div>
+      );
+    }
+    if (refDragSelector.current.addSelectables) {
+      refDragSelector.current.addSelectables(
+        document.getElementsByClassName("selectable_bits")
       );
     }
 
